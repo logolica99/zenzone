048665c854e35c4ca9499aef7ca6e6b855108a79
Jubaer Jami <jamijubaer@gmail.com>
2023-05-25 23:31:55 +0600

    fixed build bug 1

--- src/app/auth/login/layout.tsx
+++ src/app/auth/login/layout.tsx
@@ -4,17 +4,14 @@ import React from "react";
 
 import "../../globals.css";
 import { Inter } from "next/font/google";
-import { Metadata } from "next";
+
 
 import { SessionProvider } from "next-auth/react";
 import { Session } from "next-auth";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Zenzone",
-  description: "Unleash your Zen Mode!",
-};
+
 
 export default function RootLayout({
   children,
